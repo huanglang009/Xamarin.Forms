e3e1ed0bcb4ff72abaa2ecdab7a65f54032c96e5
ShaneN <shane94@hotmail.com>
2018-11-20 10:06:53 -0700

    Visual

--- Xamarin.Forms.Core/Properties/AssemblyInfo.cs
+++ Xamarin.Forms.Core/Properties/AssemblyInfo.cs
@@ -105,3 +105,4 @@ using Xamarin.Forms.StyleSheets;
 [assembly: StyleProperty("-xf-thumb-color", typeof(Slider), nameof(Slider.ThumbColorProperty))]
 [assembly: StyleProperty("-xf-spacing", typeof(StackLayout), nameof(StackLayout.SpacingProperty))]
 [assembly: StyleProperty("-xf-orientation", typeof(StackLayout), nameof(StackLayout.OrientationProperty))]
+[assembly: StyleProperty("-xf-visual", typeof(VisualElement), nameof(VisualElement.VisualProperty))]
